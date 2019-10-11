83442c806b26ad87ddea299d227cd2bcdfee90cc
Jim Speir <jimbo80982@gmail.com>
2019-10-12 00:26:25 +0100

    DNS changes are now being saved properly

--- src/dns_plugins/ovh.ts
+++ src/dns_plugins/ovh.ts
@@ -1,3 +1,4 @@
+var request = require('request-promise');
 import IDNSPlugin from "./interface";
 import { readlink } from "fs";
 import {
@@ -11,20 +12,22 @@ import * as Promise from "bluebird";
 import { 
     DomainMap,
     OVHError,
-    OVHZoneRecord
+    OVHZoneRecord,
+    OVHConfig,
+    AuthToken
  } from "../types";
 
 
 export default class OVHDNSPlugin implements IDNSPlugin
 {
 
-    private _config;
+    private _config:OVHConfig;
     private _ovh;
     private _domainMap:DomainMap;
-    private _consumerKey;
 
     constructor(config)
     {
+        let _consumerKey:string = null;
         this._config = config;
         this._ovh = require('ovh')({
             appKey: this._config.appKey,
@@ -70,14 +73,28 @@ export default class OVHDNSPlugin implements IDNSPlugin
 
         let keyAuthDigest = makeChallengeKeyAuthDigest(keyAuthorisation);
         this.setRecord(foundDomain.domain,"TXT",acmePath,keyAuthDigest,600)
-        .then();
+        .then(() => {
+            return this.getRecord(foundDomain.domain,"NS",foundDomain.name)
+                .then((dnsList:OVHZoneRecord[]) =>{
+                    console.log(dnsList);
+                    //let nsList = _.map(nameservers, "data");
+                    //return lookupIPs(nsList)
+                    cb(null)
+                })
+                .catch((err) => {
+                    return Promise.reject(`[ovh] An error occurred whilst getting the nameserver records on ${foundDomain.domain}`);
+                })
+            
+        })
+        .catch((err) => cb(err));
     }
-
+    
     get(args: any, domain: string, challenge: any, cb: any) {
         //Empty on the godaddy plugin
     }
 
     remove(args: any, domain: string, challenge: any, cb: any) {
+        console.log(`Removing record from ${domain} on OVH`);
         let foundDomain = getDomainAndNameFromMap(this._domainMap, domain);
         if (!foundDomain || !foundDomain.name || !foundDomain.domain) {
             return cb(new Error(`The requested domain ${domain} is not available in the godaddy instance configured`));
@@ -95,15 +112,14 @@ export default class OVHDNSPlugin implements IDNSPlugin
     }
 
     setIPv4(host: string, ips: string[]):Promise<any> {
+        console.log(`[ovh] Setting IPv4 addresses`);
         let foundDomain = getDomainAndNameFromMap(this._domainMap, host);
         if (!foundDomain || !foundDomain.name || !foundDomain.domain) {
-            return Promise.reject(new Error(`The requested domain ${host} is not available in the godaddy instance configured`));
+            console.log(`[ovh] The requested domain is not available`)
+            return Promise.reject(new Error(`The requested domain ${host} is not available in the ovh instance configured`));
         }
         
         console.log(`Looking for ${host} and found ${foundDomain.name} with domain ${foundDomain.domain}`);
-        if (foundDomain.name === foundDomain.domain) {
-            foundDomain.name = "@";
-        }
         
         //Queue the OVH requests for all IPs
         let multiReq = ips.map((ip: string) => {
@@ -120,9 +136,6 @@ export default class OVHDNSPlugin implements IDNSPlugin
         }
         
         console.log(`Looking for ${host} and found ${foundDomain.name} with domain ${foundDomain.domain}`);
-        if (foundDomain.name === foundDomain.domain) {
-            foundDomain.name = "@";
-        }
         
         //Queue the OVh requests for all IPs
         let multiReq = ips.map((ip: string) => {
@@ -132,6 +145,12 @@ export default class OVHDNSPlugin implements IDNSPlugin
     }
 
     setRecord(domain: string, type: string, name: string, value: string, ttl: number) : Promise<any> {
+        if(name == domain)
+        {
+            //When using OVH, the provided name is a subdomain and therefore should simply be blank when it's for the current domain
+            name = '';
+        }
+        console.log(`[ovh] Adding a new ${type} record for ${domain} with the name ${name} with destination ${value} and TTL ${ttl}`);
         return this._ovh.requestPromised('POST',`/domain/zone/${domain}/record`,{
             fieldType: type,
             subDomain: name,
@@ -142,12 +161,13 @@ export default class OVHDNSPlugin implements IDNSPlugin
             return Promise.reject(`[ovh] Error: ${err.message}`);
         })
         .then((record: OVHZoneRecord) => {
+            console.log(`[ovh] Made DNS change`)
             //Refresh call just applies the edited zone, returns void
             return this.applyDNSChanges(domain);
         });
     }
 
-    getRecord(domain: string, type: string, name: string) : Promise<OVHZoneRecord>{
+    getRecord(domain: string, type: string, name: string) : Promise<OVHZoneRecord[]>{
         return this._ovh.requestPromised('GET',`/domain/zone/${domain}/record`,{
             fieldType: type,
             subdomain: name
@@ -157,11 +177,17 @@ export default class OVHDNSPlugin implements IDNSPlugin
             {
                 case 1: return this._ovh.requestPromised('GET',`/domain/zone/${domain}/record/${idlist[0]}`)
                     .then((record:OVHZoneRecord) => {
-                        return record;
+                        return [record];
                     });
 
                 case 0: return Promise.reject(`[ovh] ${type} record named ${name} is not listed on ${domain}`);
-                default: return Promise.reject(`[ovh] ${type} record named ${name} is listed numerous times on ${domain}`);
+                default: 
+                    return Promise.all(idlist.map((id) => this._ovh.requestPromised('GET',`/domain/zone/${domain}/record/${id}`)))
+                        .then((results:any) => {
+                            console.log(results);
+                            return results;
+                        });
+                return ;
             }
         })
         .catch((err:OVHError) => {
@@ -171,20 +197,35 @@ export default class OVHDNSPlugin implements IDNSPlugin
 
     removeRecord(domain: string, type: string, name: string): Promise<any> {
         return this.getRecord(domain,type,name)
-            .then((record: OVHZoneRecord) => {
-                return this._ovh.requestPromise('DELETE',`/domain/zone/${record.zone}/record/${record.id}`)
-                    .then(() => {
-                        return this.applyDNSChanges;
-                    })
-                    .catch((err:OVHError) => {
-                        return Promise.reject(`[ovh] Error: Failed to delete record ID ${record.id} from zone ${record.zone}`);
-                    });
+            .then((records: OVHZoneRecord[]) => {
+                switch(records.length)
+                {
+                    case 1:
+                        let record = records[0];
+                        return this._ovh.requestPromise('DELETE',`/domain/zone/${record.zone}/record/${record.id}`)
+                            .then(() => {
+                                return this.applyDNSChanges;
+                            })
+                            .catch((err:OVHError) => {
+                                return Promise.reject(`[ovh] Error: Failed to delete record ID ${record.id} from zone ${record.zone}`);
+                            });
+
+                    case 0:
+                        return Promise.reject(`[ovh] Couldn't find ${type} record with name ${name} on ${domain}`);
+
+                    default:
+                        return Promise.reject(`[ovh] Multiple ${type} records of name ${name} marked for deletion on ${domain}, unsupported`)
+
+
+                }
             });
     }
 
     applyDNSChanges(domain:string):Promise<void>{
-        return this._ovh.requestPromised('POST',`/domain/zone/${domain}/refresh`,{
-            zoneName: domain
+        return this._ovh.requestPromised('POST',`/domain/zone/${domain}/refresh`)
+        .then((res) => {
+            console.log(`[ovh] DNS changes saved`);
+            return Promise.resolve();
         })
         .catch((err:OVHError) => {
             return Promise.reject(`[ovh] Error: ${err.message}`);
--- src/types.ts
+++ src/types.ts
@@ -15,3 +15,16 @@ export type OVHZoneRecord = {
     ttl:number;
     id:number; //OVH returns an ID with it's records
 }
+
+export type AuthToken = {
+    validationUrl:string;
+    consumerKey:string;
+    state:string;
+}
+
+export type OVHConfig = {
+    appKey:string;
+    appSecret:string;
+    consumerKey:string;
+    endpoint:string;
+}
